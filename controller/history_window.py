cfb28b307ffdc21ca66e68d8f0050f05e0b08a02
PedroGregori <ferreiragregorimelo@gmail.com>
2022-09-23 19:59:20 -0300

    some Ui changes

--- controller/history_window.py
+++ controller/history_window.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QWidget, QTableWidgetItem, QHeaderView
 from PyQt5 import uic
 
 FILE_UI = 'view/history_window.ui'
@@ -7,3 +7,13 @@ class history_window(QWidget):
     def __init__(self):
         super(history_window, self).__init__()
         uic.loadUi(FILE_UI, self)
+        
+        self.table.horizontalHeader().setStretchLastSection(True)
+        self.table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.table.horizontalHeader().setSectionResizeMode(
+            0, QHeaderView.ResizeToContents)
+        
+        self.table2.horizontalHeader().setStretchLastSection(True)
+        self.table2.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.table2.horizontalHeader().setSectionResizeMode(
+            1, QHeaderView.ResizeToContents)

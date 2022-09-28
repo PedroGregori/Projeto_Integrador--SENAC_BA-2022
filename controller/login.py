89c98aef335fbe19fdf5cb0ce6c5091e2ba33472
PedroGregori <ferreiragregorimelo@gmail.com>
2022-09-28 19:49:25 -0300

    commit

--- controller/login.py
+++ controller/login.py
@@ -7,3 +7,10 @@ class Login_ui(QWidget):
     def __init__(self):
         super(Login_ui, self).__init__()
         uic.loadUi(FILE_UI, self)
+        
+        self.Btn_enter.clicked.connect(self.enter)
+        
+    def enter(self):
+        login = self.login.text()
+        password = self.password.text()
+        
